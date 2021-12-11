f52e81ef59cc2fa109ac7a76fb8a37abad29b1e0
Alberto de Diego <albertousansolo@gmail.com>
2021-12-11 11:49:38 +0100

    dfgdfg

--- MarcosCore/TssCalculator.cs
+++ MarcosCore/TssCalculator.cs
@@ -20,14 +20,30 @@ namespace MarcosCore
 
         private class Rango
         {
-            public int Puntos { get; private set; }
-            public int Porcentaje { get; private set; }
+            public decimal Puntos { get; private set; }
+            public decimal Porcentaje { get; private set; }
 
-            public Rango(int porc, int points)
+            public Rango(decimal porc, decimal points)
             {
                 Porcentaje = porc;
                 Puntos = points;
             }
+        }
+
+        private List<Rango> GetRangos(int inicioPorc, decimal finPorc, decimal puntosMin, decimal puntosMax)
+        {
+            var lista = new List<Rango>();
+            for (decimal i = inicioPorc; i <= finPorc; i += 0.1M)
+            {
+                var puntosADividir = puntosMax - puntosMin;
+                var miPunto = i - inicioPorc;
+                var miPorc = miPunto * 100 / (finPorc - inicioPorc);
+                var miValor = miPorc * puntosADividir / 100;
+
+                lista.Add(new Rango(i, miValor + puntosMin));
+
+            }
+            return lista;
         }
 
 
@@ -36,57 +52,28 @@ namespace MarcosCore
 
             try
             {
-                var listaLimites = new List<Rango>
-                {
-                    new Rango(0, 20),
-                    new Rango(61, 30),
-                    new Rango(71, 40),
-                    new Rango(81, 50),
-                    new Rango(85, 60),
-                    new Rango(90, 70),
-                    new Rango(94, 80),
-                    new Rango(100, 100),
-                    new Rango(103, 120),
-                    new Rango(106, 140)
-                };
-
-
-
-                var listaDatos = new List<Rango>();
-                var conta = 0;
-                while (conta <= 100)
-                {
-                    if (conta < 60)
-                    {
-                        listaDatos.Add(new Rango(conta, 20));
-                        conta++;
-                        continue;
-                    }
-                    var mirango = listaLimites.Last(i => i.Porcentaje < conta);
-                    var rangosuperior = listaLimites.First(i => i.Porcentaje > conta);
-                    //hay que sacar el porcentaje
-                    var diferenciaPuntos = rangosuperior.Puntos - mirango.Puntos;
-                    var diferenciaPorc = rangosuperior.Porcentaje - mirango.Porcentaje;
-                    var PuntoPorPorc = diferenciaPuntos / diferenciaPorc;
-
-                    var miPorcentajeEnNivel = rangosuperior.Porcentaje - conta;
-                    var añadoenNivel = miPorcentajeEnNivel * PuntoPorPorc;
-                    var totalPorc = añadoenNivel + mirango.Puntos;
-
-                    listaDatos.Add(new Rango(conta, totalPorc));
-
-                    conta++;
-                }
+                var listaLimites = new List<Rango>();
+                listaLimites.AddRange(GetRangos(1, 60.9M, 20, 30));
+                listaLimites.AddRange(GetRangos(61, 70.9M, 30, 40));
+                listaLimites.AddRange(GetRangos(71, 80.9M, 40, 50));
+                listaLimites.AddRange(GetRangos(81, 84.9M, 50, 60));
+                listaLimites.AddRange(GetRangos(85, 89.9M, 60, 70));
+                listaLimites.AddRange(GetRangos(90, 93.9M, 70, 80));
+                listaLimites.AddRange(GetRangos(94, 99.9M, 80, 100));
+                listaLimites.AddRange(GetRangos(100, 102.9M, 100, 120));
+                listaLimites.AddRange(GetRangos(103, 106.9M, 120, 140));
+                listaLimites.AddRange(GetRangos(106, 150.9M, 140, 200));
 
                 var mifth = decimal.Parse(model.FTHR);
                 var mispulsacionesmedia = decimal.Parse(model.PulsacionesMedias);
                 var miporcentaje = mispulsacionesmedia * 100 / mifth;
-                var mirangoSeleccionado = listaDatos.First(i => i.Porcentaje == Math.Round(miporcentaje, 0, MidpointRounding.AwayFromZero));
+                var miProcentajeConUndecimal = Math.Round(miporcentaje, 1, MidpointRounding.AwayFromZero);
+                var mirangoSeleccionado = listaLimites.First(i => i.Porcentaje == miProcentajeConUndecimal);
                 var misPuntos = mirangoSeleccionado.Puntos;
                 var miPorcentaje = mirangoSeleccionado.Porcentaje;
                 var mishoras = decimal.Round(decimal.Parse(model.Minutos) / 60, 2);
 
-                model.Result= $"{decimal.Round(miporcentaje, 1)} % -> {misPuntos} TSS * {mishoras} horas=> TOTAL {decimal.Round(misPuntos * mishoras, 2)} TSS en Golden Ch.";
+                model.Result= $"{miProcentajeConUndecimal} % -> {decimal.Round(misPuntos,2)} TSS * {mishoras} horas=> TOTAL {decimal.Round(misPuntos * mishoras, 2)} TSS en Golden Ch.";
             }
           catch(Exception exc)
             {
